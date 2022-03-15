1be3e469b83b99fe8b049383e87172df6381fc4f
Alin-Gabriel Drăghici <guky667@gmail.com>
2022-03-15 15:56:56 +0200

    Update wordle.py
    Updated var names

--- wordle.py
+++ wordle.py
@@ -4,83 +4,72 @@
 # region Vars and inits
 # SERAI _should_ be the best starting word
 
-wpl = 20 # Words Per Line
-counter = 0
-first = []
-second = []
-third = []
-fourth = []
-fifth = []
-alph = []
+words_per_line = 20
+print_counter = 0
+green1 = []
+green2 = []
+green3 = []
+green4 = []
+green5 = []
+all_letters = []
 #endregion
 
 ### BLACK LETTERS 
 # letters to avoid, that are not part of the word
-avoid = ['R','A','I','N','D']
+black_letters = ['R','A','I','N','D','L','T','H']
 
 # generate the entire ALPHabet (of uppercase letters)
-for i in range(65, 91):
-    if (chr(i) not in avoid):
-        alph.append(chr(i))
-first = second[:] = third[:] = fourth[:] = fifth[:] = alph[:]
+for char in range(65, 91):
+    if (chr(char) not in black_letters):
+        all_letters.append(chr(char))
+green1 = green2[:] = green3[:] = green4[:] = green5[:] = all_letters[:]
 
 ### YELLOW LETTERS
-# remove letters on specific spots
-firstR = ['']
-secondR = ['E','P']
-thirdR = ['']
-fourthR = ['']
-fifthR = ['']
+# add letters to be removed from specific positions
+yellow1 = ['']
+yellow2 = ['E','P']
+yellow3 = ['']
+yellow4 = ['']
+yellow5 = ['']
 
 ### GREEN LETTERS
-# set letters that have been found in the right spot - uncomment and add letter
-first = ['S']
-#second = ['']
-third = ['E']
-#fourth = ['']
-#fifth = ['']
+# set letters that have been found in the right position - uncomment and add letter
+green1 = ['S']
+#green2 = ['']
+green3 = ['E']
+green4 = ['E']
+green5 = ['P']
 
 #region Execution
 # create list of letters to include based on Yellow Letters
-include = list(dict.fromkeys(','.join(filter(None,[*firstR, *secondR, *thirdR, *fourthR, *fifthR])).split(',')))
+include = list(dict.fromkeys(','.join(filter(None,[*yellow1, *yellow2, *yellow3, *yellow4, *yellow5])).split(',')))
 
-# check that input data is correct
-exclude = [firstR[0], secondR[0], thirdR[0], fourthR[0], fifthR[0]]
-
-for letter in include:
-    if letter in avoid:
-        print ('Cannot avoid and include the same letter: ', letter)
-        print ('Please remove', letter, 'from either the include or exclude list')
-        raise SystemExit(0)
-
-counter = 0
-
-for f in first:
-    if (len(first) != 1):
-        print(end = '\n\n')
+for first_letter in green1:
+    if (len(green1) != 1):
+        print(1,end = '\n\n')
         counter = 0
-    if f not in firstR:
-        for s in second:
-            if (len(first) == 1):
-                print(end = '\n\n')
-                counter = 0
-            if s not in secondR:
-                for t in third:
-                    if (len(second) == 1 and len(first) == 1):
-                        print(end = '\n\n')
-                        counter = 0
-                    if t not in thirdR:
-                        for fo in fourth:
-                            if (len(third) == 1 and len(second) == 1 and len(first) == 1):
-                                print(end = '\n\n')
-                                counter = 0
-                            if fo not in fourthR:
-                                for fi in fifth:
-                                    if (len(fourth) == 1 and len(third) == 1 and len(second) == 1 and len(first) == 1):
-                                        print(end = '\n\n')
-                                        counter = 0
-                                    if fi not in fifthR:
-                                        word = f + s + t + fo + fi
+    if first_letter not in yellow1:
+        for second_letter in green2:
+            if second_letter not in yellow2:
+                if (len(green2) != 1 and len(green1) == 1):
+                    print(end = '\n\n')
+                    print_counter = 0
+                for third_letter in green3:
+                    if third_letter not in yellow3:
+                        if (len(green3) != 1 and len(green2) == 1 and len(green1) == 1):
+                            print(end = '\n\n')
+                            print_counter = 0
+                        for fourth_letter in green4:
+                            if fourth_letter not in yellow4:
+                                if (len(green4) != 1 and len(green3) == 1 and len(green2) == 1 and len(green1) == 1):
+                                    print(end = '\n\n')
+                                    print_counter = 0
+                                for fifth_letter in green5:
+                                    if fifth_letter not in yellow5:
+                                        if (len(green5) != 1 and len(green4) == 1 and len(green3) == 1 and len(green2) == 1 and len(green1) == 1):
+                                            print(end = '\n\n')
+                                            print_counter = 0
+                                        word = first_letter + second_letter + third_letter + fourth_letter + fifth_letter
                                         valid = True
                                         for letter in include:
                                             if letter not in word:
@@ -88,14 +77,17 @@ for f in first:
                                         if valid == True:
                                             # rules
                                             if (
-                                                not(f==s==t or s==t==fo or t==fo==fi)
-                                                and ('XS' not in word)
-                                                and (fi!='V' and fi!='J')
-                                                and not('Q' in word and 'QU' not in word)
-                                            ):
-                                                counter += 1
+                                                not(
+                                                    first_letter==second_letter==third_letter or
+                                                    second_letter==third_letter==fourth_letter or
+                                                    third_letter==fourth_letter==fifth_letter
+                                                )
+                                            and ('XS' not in word)
+                                            and (fifth_letter not in ['V','J'])
+                                            and not('Q' in word and 'QU' not in word)):
+                                                print_counter += 1
                                                 print (word, end = ' ')
-                                                if counter == wpl:
-                                                    counter = 0
+                                                if print_counter == words_per_line:
+                                                    print_counter = 0
                                                     print()
 #endregion
