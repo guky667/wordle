aa7bb78f9e68828ed7705f09caee6e931b4d07fc
Alin-Gabriel Drăghici <guky667@gmail.com>
2022-03-16 03:04:48 +0200

    Update wordle.py

--- wordle.py
+++ wordle.py
@@ -4,7 +4,7 @@
 # region Vars and inits
 # SERAI _should_ be the best starting word
 
-words_per_line = 20
+words_per_line = 24
 print_counter = 0
 all_letters = []
 pivot_letter = ''
@@ -12,7 +12,7 @@ pivot_point = 0
 #endregion
 
 ### BLACK LETTERS - all inline
-black_letters = 'SERIKTB'
+black_letters = 'SERIKTBD'
 
 ### YELLOW LETTERS - per positions
 yellow1 = 'L'
@@ -22,10 +22,10 @@ yellow4 = 'A'
 yellow5 = ''
 
 ### GREEN LETTERS - per positions
-green1 = ''
-green2 = ''
+green1 = 'M'
+green2 = 'A'
 green3 = ''
-green4 = ''
+green4 = 'L'
 green5 = 'Y'
 
 #region Execution
@@ -37,9 +37,9 @@ if green2 == '': green2 = [second_letter for second_letter in all_letters if sec
 if green3 == '': green3 = [third_letter for third_letter in all_letters if third_letter not in yellow3]
 if green4 == '': green4 = [fourth_letter for fourth_letter in all_letters if fourth_letter not in yellow4]
 if green5 == '': green5 = [fifth_letter for fifth_letter in all_letters if fifth_letter not in yellow5 and fifth_letter not in 'VJ']
-
 # create list of letters to include based on Yellow Letters
 include = ''.join(set(yellow1 + yellow2 + yellow3 + yellow4 + yellow5))
+
 
 # Bad input check
 for yellow_letter in include:
@@ -62,14 +62,16 @@ words = [
             first_letter == second_letter == third_letter or
             second_letter == third_letter == fourth_letter or
             third_letter == fourth_letter == fifth_letter
-        ) and (
-            first_letter in include or
-            second_letter in include or
-            third_letter in include or
-            fourth_letter in include or
-            fifth_letter in include 
         )
     )]
+
+for letter in include:
+    for word in words[:]:
+        if (letter not in word):
+            try:
+                words.remove(word)
+            except:
+                pass
 
 # Filtering words based on grammar rules
 words = [word for word in words 
