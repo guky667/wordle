9d5c3e6ebd62a4bb5c6a42dcf62e50fb761bb36b
Alin-Gabriel Drăghici <guky667@gmail.com>
2022-03-17 19:42:25 +0200

    Update wordle.py
    performance improvements

--- wordle.py
+++ wordle.py
@@ -6,22 +6,24 @@
 # "SERAI" _should_ be the 
 # best starting word
 
+import datetime
+
 words_per_line = 26
 print_counter = 0
-all_letters = []
+all_letters = ()
 pivot_letter = ''
 pivot_point = 0
 #endregion
 
 ### BLACK LETTERS
-black_letters = 'SEROCKLP'
+black_letters = ''
 
 ### YELLOW LETTERS
 yellow1 = ''
-yellow2 = 'T'
+yellow2 = ''
 yellow3 = ''
-yellow4 = 'AM'
-yellow5 = 'I'
+yellow4 = ''
+yellow5 = ''
 
 ### GREEN LETTERS
 green1 = ''
@@ -30,15 +32,17 @@ green3 = ''
 green4 = ''
 green5 = ''
 
+now = datetime.datetime.now()
+
 #region Execution
 # generate the entire alphabet (of uppercase letters) and base lists of letter combinations 
-all_letters = [chr(char) for char in range(65,91) if chr(char) not in ''.join(set(black_letters))]
+all_letters = [chr(char) for char in range(65,91) if chr(char) not in ''.join(black_letters)]
 
 if green1 == '': green1 = [first_letter for first_letter in all_letters if first_letter not in yellow1]
 if green2 == '': green2 = [second_letter for second_letter in all_letters if second_letter not in yellow2]
 if green3 == '': green3 = [third_letter for third_letter in all_letters if third_letter not in yellow3]
 if green4 == '': green4 = [fourth_letter for fourth_letter in all_letters if fourth_letter not in yellow4]
-if green5 == '': green5 = [fifth_letter for fifth_letter in all_letters if fifth_letter not in yellow5 and fifth_letter not in 'VJ']
+if green5 == '': green5 = [fifth_letter for fifth_letter in all_letters if fifth_letter not in yellow5 and fifth_letter not in 'VJQ']
 
 # Choosing a pivot point for formatting
 if (len(green1) == 1): pivot_point = 1
@@ -53,47 +57,47 @@ for yellow_letter in include:
         raise SystemExit(0)
 
 # Define function for filtering out invalid words
-def filter_out(word):
-    if ('KK' in word or 'QQ' in word or 'UU' in word or 'YY' in word or 'HH' in word):
-        return False
-    if (
-        word[0] == word[1] == word[2] or
-        word[1] == word[2] == word[3] or
-        word[2] == word[3] == word[4]
-    ):
-        return False
-    if ((word[0] == 'Q' and word[1] != 'U') or
-        (word[1] == 'Q' and word[2] != 'U') or
-        (word[2] == 'Q' and word[3] != 'U') or
-        (word[3] == 'Q' and word[4] != 'U')
-    ):
-        return False
-    if ('BQ' in word or 'CJ' in word or 'CV' in word or 'FZ' in word or 'GQ' in word or 'JQ' in word or
-        'JV' in word or 'JX' in word or 'KQ' in word or 'PQ' in word or 'XZ' in word or 'QB' in word or
-        'JC' in word or 'VC' in word or 'ZF' in word or 'QG' in word or 'QJ' in word or 'VJ' in word or 
-        'XJ' in word or 'QK' in word or 'QP' in word or 'ZX' in word or 'VP' in word or 'XS' in word
-    ):
-        return False
-    for letter in include:
-        if (letter not in word):
-            return False
-    return True
+def filter_out(words):
+    filtered_words = []
+    for word in words:
+        for letter in include:
+            if (letter not in word):
+                continue
+        if (
+            word[0] == word[1] == word[2] or
+            word[1] == word[2] == word[3] or
+            word[2] == word[3] == word[4] or
+            ('Q' in word and word[word.index('Q') + 1] != 'U')
+        ):
+            continue
+        if ('BQ' in word or 'CJ' in word or 'CV' in word or 'FZ' in word or 'GQ' in word or 'JQ' in word or
+            'JV' in word or 'JX' in word or 'KQ' in word or 'PQ' in word or 'XZ' in word or 'QB' in word or
+            'JC' in word or 'VC' in word or 'ZF' in word or 'QG' in word or 'QJ' in word or 'VJ' in word or 
+            'XJ' in word or 'QK' in word or 'QP' in word or 'ZX' in word or 'VP' in word or 'XS' in word or
+            'KK' in word or 'QQ' in word or 'UU' in word or 'YY' in word or 'HH' in word
+        ):
+            continue
+        filtered_words.append(word)
+    return filtered_words
 
 # Generating a list of words
-words = [
-    first_letter + second_letter + third_letter + fourth_letter + fifth_letter
-
+unfiltered_words = [
+    ''.join([first_letter, second_letter, third_letter, fourth_letter, fifth_letter])
     for first_letter in green1 
     for second_letter in green2
     for third_letter in green3 
     for fourth_letter in green4
     for fifth_letter in green5
-
-    if(filter_out(first_letter + second_letter + third_letter + fourth_letter + fifth_letter))
 ]
 
+filtered_words = filter_out(unfiltered_words)
+
+
+print (datetime.datetime.now() - now)
+raise SystemExit(0)
+
 # Printing the words
-for word in words:
+for word in filtered_words:
     if pivot_letter != word[pivot_point]:
         print(end = '\n\n')
         print_counter = 0
@@ -104,3 +108,5 @@ for word in words:
     pivot_letter = word[pivot_point]
     print_counter += 1
 #endregion
+
+print (datetime.datetime.now() - now)
